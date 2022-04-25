a1723fc24725688663a768a0120807068f4dc122
sohial19 <sohailshaik1910@gmail.com>
2022-04-25 18:50:13 +0530

    Here we created single line argument as a string varable

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[] args){
+        if(args.length>0){
+        for(String val: args)
+        System.out.println(val);
+    }
+        else{
+
+            System.out.println("There is no arguments are provided");
+
+        
+    }
+
+    }
+}
