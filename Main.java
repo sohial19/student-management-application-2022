2dd43b97ca4581b1498e1e13fc7799789d523ce5
kadiri uday <udaykumarkadiri933@gmail.com>
2022-05-04 15:41:35 +0530

    replaced loop and given one argument

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         if (args.length > 0) {
-            for (String val : args)
-                System.out.println(val);
+            System.out.println(args[0]);
         } else {
             System.out.println("No argumnet given");
         }
